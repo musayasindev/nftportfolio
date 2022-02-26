29bb7750564cc3b21fc03699b451c366c8719699
musayasindev <musayasin12345678@gmail.com>
2022-02-26 14:33:00 +0500

    update

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+                <link href="../css/styles.css" rel="stylesheet" />
+
+    <q-header elevated class="bg-dark text-white flex flex-center">
+
+
+
+                  <q-title class="q-pa-md">
+
+                       Nft Portfolio (by Musayasin) 
+
+                  </q-title>
+
+
+
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+
+<script>
+import { defineComponent } from 'vue';
+import '../css/styles.css'
+
+export default defineComponent({
+  name: 'NavBar'
+})
+
+</script>
